351fb021a56c6e7e5ef0baccd1968be3f783722a
Serik2910 <70191204+Serik2910@users.noreply.github.com>
2021-08-21 21:59:44 +0600

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");//comment fdfg
+            Console.WriteLine("Hello World!");//comment fdfg1
         }
     }
 }
